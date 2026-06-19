712bc9c33870ccb73f4a23620a3e343747782f6d
Bbeaurai <beaurainbenjamin1@gmail.com>
2026-06-19 17:11:07 +0200

    add

--- src/indexing.py
+++ src/indexing.py
@@ -7,8 +7,13 @@
 #   By: bbeaurai <bbeaurai@student.42lehavre.fr>     +#+  +:+       +#+       #
 #                                                  +#+#+#+#+#+   +#+          #
 #   Created: 2026/06/19 11:28:05 by bbeaurai            #+#    #+#            #
-#   Updated: 2026/06/19 11:28:09 by bbeaurai           ###   ########.fr      #
+#   Updated: 2026/06/19 16:14:38 by bbeaurai           ###   ########.fr      #
 #                                                                             #
 # ########################################################################### #
 
 pass
+# BM25 = Best match 25
+
+# prend en compte plusieur elements
+# la frequence des termes de la requete apparaissent dans le codument
+# la lontueur du doc
