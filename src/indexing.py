f49a9d97e4d7229c79366eade08855efff6064d2
Bbeaurai <beaurainbenjamin1@gmail.com>
2026-06-20 09:27:19 +0200

    add

--- src/indexing.py
+++ src/indexing.py
@@ -7,11 +7,12 @@
 #   By: bbeaurai <bbeaurai@student.42lehavre.fr>     +#+  +:+       +#+       #
 #                                                  +#+#+#+#+#+   +#+          #
 #   Created: 2026/06/19 11:28:05 by bbeaurai            #+#    #+#            #
-#   Updated: 2026/06/19 16:14:38 by bbeaurai           ###   ########.fr      #
+#   Updated: 2026/06/19 17:18:33 by bbeaurai           ###   ########.fr      #
 #                                                                             #
 # ########################################################################### #
 
 pass
+
 # BM25 = Best match 25
 
 # prend en compte plusieur elements
